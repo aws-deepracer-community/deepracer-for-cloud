b3796e837ae300c9f69bcf6160b68128ac4729a3
Michael Equi <equim20@studnet.jhs.net>
2019-07-26 12:10:15 -0700

    Added memory manager and cleaned up start script

---
+++ scripts/training/memoryManager.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+import time
+
+
+def get_folder_size(folder):
+    total_size = os.path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = os.path.join(folder, item)
+        if os.path.isfile(itempath):
+            total_size += os.path.getsize(itempath)
+        elif os.path.isdir(itempath):
+            total_size += get_folder_size(itempath)
+    return total_size
+
+
+def handle_folder_files(path, name, max_size):
+    if os.path.exists(path):
+        size = get_folder_size(path) / 1000000000  # directory size in GB
+        print("Size of " + name + " path directory: " + str(size) + "GB" + " capped at " + str(max_size) + "GB")
+
+        if size > max_size:
+            file_list = []
+            for file in os.listdir(path):
+                file_list.append([os.stat(os.path.join(path, file)).st_mtime, os.path.join(path, file)])
+            file_list.sort(key=lambda x: x[0])  # sort by creation date, delete the old files first
+            if os.path.basename(file_list[0][1]) != "model_metadata.json":
+                os.remove(file_list[0][1])
+                print("Removed: " + os.path.basename(file_list[0][1]))
+            else:
+                os.remove(file_list[1][1])
+                print("Removed: " + os.path.basename(file_list[1][1]))
+        print("")
+
+
+def manage_memory(args):
+    model_path = os.path.join(os.path.abspath(""), "../../docker/volumes/minio/bucket/rl-deepracer-sagemaker/model/")
+    checkpoint_path = os.path.join(os.path.abspath(""), "../../docker/volumes/robo/checkpoint")
+    while True:
+        handle_folder_files(model_path, "model", args.sagemaker_model_cap)
+        handle_folder_files(checkpoint_path, "checkpoint", args.checkpoint_cap)
+
+        time.sleep(30)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Manage memory usage of local DeepRacer by capping the memory usage of key folders")
+    parser.add_argument('-m', '--sagemaker_model_cap',
+                        help="The cap size (in GB) for the model folder in volumes/minio/bucket/rl-deepracer-sagemaker/model/",
+                        type=float, default=5)
+    parser.add_argument('-c', '--checkpoint_cap',
+                        help="The cap size (in GB) for the model folder in volumes/robo/checkpoint/checkpoint/",
+                        type=float, default=5)
+    args = parser.parse_args()
+    manage_memory(args)
--- scripts/training/start.sh
+++ scripts/training/start.sh
@@ -8,8 +8,11 @@ echo 'waiting for containers to start up...'
 #sleep for 20 seconds to allow the containers to start
 sleep 15
 
-echo 'attempting to pull up sagemaker logs...'
-gnome-terminal -x sh -c "!!; docker logs -f $(docker ps | awk ' /sagemaker/ { print $1 }')"
+echo 'Attempting to pull up sagemaker logs...'
+gnome-terminal -x sh -c "docker logs -f $(docker ps | awk ' /sagemaker/ { print $1 }')"
 
-echo 'attempting to open vnc viewer...'
-gnome-terminal -x sh -c "!!; vncviewer localhost:8080"
+echo 'Attempting to open vnc viewer...'
+gnome-terminal -x sh -c "echo vncviewer;vncviewer localhost:8080"
+
+echo 'Starting memory manager...'
+gnome-terminal -x sh -c "echo memory manager;python memoryManager.py"
