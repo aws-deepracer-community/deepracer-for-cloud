42b967fc51c935edc79ac6e1f2366d3cfdfdf522
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-04-15 11:00:20 +0000

    Fixing log-analysis folder permissions

--- scripts/log-analysis/start.sh
+++ scripts/log-analysis/start.sh
@@ -4,5 +4,6 @@ docker run --rm -it -p "8888:8888" \
 -v `pwd`/../../logs:/workspace/logs \
 -v `pwd`/../../docker/volumes/.aws:/root/.aws \
 -v `pwd`/../../analysis:/workspace/analysis \
+-e HOST_PERMS="$(id -u):$(id -g)" \
 --name loganalysis \
  larsll/deepracer-loganalysis:v2-cpu
