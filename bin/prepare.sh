ada1de3507aefb167b978c6283bde5f18f539f53
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-04-15 18:12:42 +0000

    Fix missing sudo in script

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -104,7 +104,7 @@ sudo apt-get install -y awscli python3-boto3
 ## Installing Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 if [[ "${ARCH}" == "gpu" ]];
 then
