5e3da0fa26fee3fbbfb2a7700b22341c63725d76
Lars Lorentz Ludvigsen <59617571+larsll@users.noreply.github.com>
2020-06-16 22:37:24 +0200

    Enabling adjustment of  round robin start pos (#52)

--- scripts/training/prepare-config.py
+++ scripts/training/prepare-config.py
@@ -39,8 +39,9 @@ config['NUMBER_OF_TRIALS'] = os.environ.get('DR_EVAL_NUMBER_OF_TRIALS', '5')
 config['DISPLAY_NAME'] = os.environ.get('DR_DISPLAY_NAME', 'racer1')
 config['RACER_NAME'] = os.environ.get('DR_RACER_NAME', 'racer1')
 
-config['ALTERNATE_DRIVING_DIRECTION'] = os.environ.get('DR_ALTERNATE_DRIVING_DIRECTION', 'false')
-config['CHANGE_START_POSITION'] = os.environ.get('DR_CHANGE_START_POSITION', 'true')
+config['ALTERNATE_DRIVING_DIRECTION'] = os.environ.get('DR_TRAIN_ALTERNATE_DRIVING_DIRECTION', os.environ.get('DR_ALTERNATE_DRIVING_DIRECTION', 'false'))
+config['CHANGE_START_POSITION'] = os.environ.get('DR_TRAIN_CHANGE_START_POSITION', os.environ.get('DR_CHANGE_START_POSITION', 'true'))
+config['ROUND_ROBIN_ADVANCE_DIST'] = os.environ.get('DR_TRAIN_ROUND_ROBIN_ADVANCE_DIST', '0.05')
 config['ENABLE_DOMAIN_RANDOMIZATION'] = os.environ.get('DR_ENABLE_DOMAIN_RANDOMIZATION', 'false')
 
 # Object Avoidance
