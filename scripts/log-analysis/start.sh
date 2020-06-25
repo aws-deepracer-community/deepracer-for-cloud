fe85fc16e8a8a657f6a996a50faf0e2f5b20c07e
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-06-25 23:10:03 +0200

    Adding loganalysis to sagemaker-local network

--- scripts/log-analysis/start.sh
+++ scripts/log-analysis/start.sh
@@ -5,4 +5,5 @@ docker run --rm -it -p "8888:8888" \
 -v `pwd`/../../docker/volumes/.aws:/root/.aws \
 -v `pwd`/../../data/analysis:/workspace/analysis \
 --name loganalysis \
+--network sagemaker-local \
  larsll/deepracer-loganalysis:v2-cpu
