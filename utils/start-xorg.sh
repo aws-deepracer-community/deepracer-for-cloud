34643d89268ed77ad435a7c3790fad9e28930e25
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-07-26 21:05:58 +0200

    Remove need for shared memory

--- utils/start-xorg.sh
+++ utils/start-xorg.sh
@@ -3,7 +3,7 @@ export DISPLAY=:0
 touch ~/.Xauthority
 export XAUTHORITY=~/.Xauthority
 
-xinit /usr/bin/jwm &
+nohup xinit /usr/bin/jwm &
 sleep 1
 xrandr -s 1400x900
 x11vnc -bg -forever -nopw -rfbport 5900 -display WAIT$DISPLAY & 
