4ffe4f0dad9d0c7000aa7474f70201916ef25e85
Lars Ludvigsen <larsll@outlook.com>
2020-11-22 20:37:46 +0100

    Fixing viewer for stack

--- scripts/viewer/start.sh
+++ scripts/viewer/start.sh
@@ -54,12 +54,22 @@ server {
   }
 EOF
 echo "<html><head><title>DR-$DR_RUN_ID - $DR_LOCAL_S3_MODEL_PREFIX - $TOPIC</title><link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap\" rel=\"stylesheet\"><style>body {display: block; margin: 0; background: #161e2d; color: #ffffff; font-family: \"Roboto\",sans-serif; font-size: 16px; font-weight: 400;} .container {display: flex; flex-direction: column; position: absolute; top: 42px; bottom: 0; left: 0; right: 0;} .navbar {position: fixed; top: 0; left: 0; right: 0; z-index: 2; background: #500280} .navbar-header { font-weight: 500; font-size: 1.125rem; display: flex; flex-wrap: wrap; align-items: center; padding: 12px 16px; box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 5px -1px, rgba(0, 0, 0, 0.14) 0px 6px 10px 0px, rgba(0, 0, 0, 0.12) 0px 1px 18px 0px;} .main-container {justify-content: center; align-items: center; display: flex; flex-direction: row; flex-wrap: wrap; padding: 16px;} .card {margin: 8px; max-width: 480px; box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px; transition: box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1); border-radius: 4px; display: block; position: relative;} .card-img {border-radius: 4px;}</style></head><body><div class=\"container\"><div class=\"navbar\"><div class=\"navbar-header\">DR-$DR_RUN_ID - $DR_LOCAL_S3_MODEL_PREFIX - $TOPIC</div></div><div class=\"main-container\">" > $DR_VIEWER_HTML
-ROBOMAKER_CONTAINERS=$(docker ps --format "{{.ID}} {{.Names}}" --filter name="deepracer-${DR_RUN_ID}" | grep robomaker | cut -f1 -d\ )
+
+if [[ "${DR_DOCKER_STYLE,,}" != "swarm" ]]; then
+  ROBOMAKER_CONTAINERS=$(docker ps --format "{{.ID}} {{.Names}}" --filter name="deepracer-${DR_RUN_ID}" | grep robomaker | cut -f1 -d\ )
+else
+  ROBOMAKER_SERVICE_REPLICAS=$(docker service ps deepracer-${DR_RUN_ID}_robomaker | awk '/robomaker/ { print $1 }')
+  for c in $ROBOMAKER_SERVICE_REPLICAS; do
+    ROBOMAKER_CONTAINER_IP=$(docker inspect $c | jq -r '.[].NetworksAttachments[] | select (.Network.Spec.Name == "sagemaker-local") | .Addresses[0] ' | cut -f1 -d/)
+    ROBOMAKER_CONTAINERS="${ROBOMAKER_CONTAINERS} ${ROBOMAKER_CONTAINER_IP}"
+  done
+fi
 
 if [ -z "$ROBOMAKER_CONTAINERS" ]; then
     echo "No running robomakers. Exiting."
     exit
 fi
+
 
 for c in $ROBOMAKER_CONTAINERS; do
     C_URL="/$c/stream?topic=${TOPIC}&quality=${QUALITY}&width=${WIDTH}&height=${HEIGHT}"
