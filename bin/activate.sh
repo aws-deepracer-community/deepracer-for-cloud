570da13d8f945033c87e54fd3e90fd4b5d969e11
Lars Ludvigsen <larsll@outlook.com>
2022-11-03 22:01:37 +0100

    Workaround for minio

--- bin/activate.sh
+++ bin/activate.sh
@@ -93,6 +93,14 @@ if [[ -n "${CUDA_VISIBLE_DEVICES}" ]]; then
   echo "         and DR_SAGEMAKER_CUDA_DEVICES and rlcoach v5.0.1 or later."
 fi
 
+# Check if CUDA_VISIBLE_DEVICES is configured.
+if [[ -z "${DR_MINIO_IMAGE}" ]]; then
+  echo "WARNING: You have not configured DR_MINIO_IMAGE in system.env."
+  echo "         System will default to tag RELEASE.2022-10-24T18-35-07Z"
+  export DR_MINIO_IMAGE="RELEASE.2022-10-24T18-35-07Z"
+fi
+
+
 # Prepare the docker compose files depending on parameters
 if [[ "${DR_CLOUD,,}" == "azure" ]];
 then
