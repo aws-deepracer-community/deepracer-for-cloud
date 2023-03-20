67312fc55d2d2eb971f84d16f98dbf9d041134c8
Joshua David Magady <josh.magady@gmail.com>
2023-03-20 05:04:07 +0000

    refactor(bin): move logging library higher up

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ) # Set base Directory
 trap ctrl_c INT
 
 # Libraries
 #-----------------------------------------------------------------------------------------------------------------------
-source "$DIR"/lib/common/logging.sh
+source "$DIR"/lib/logging.sh
 source "$DIR"/lib/common/utilities.sh
 
 
