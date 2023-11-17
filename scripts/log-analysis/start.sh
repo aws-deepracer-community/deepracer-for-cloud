dba42ac5d3e8ff1e9daa77125f6f0323b9581329
Lars Ludvigsen <larsll@outlook.com>
2023-11-17 19:56:46 +0100

    Fix relative paths

--- scripts/log-analysis/start.sh
+++ scripts/log-analysis/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 docker run --rm -d -p "8888:8888" \
--v `pwd`/../../data/logs:/workspace/logs \
--v `pwd`/../../docker/volumes/.aws:/root/.aws \
--v `pwd`/../../data/analysis:/workspace/analysis \
--v `pwd`/../../data/minio:/workspace/minio \
+-v $DR_DIR/data/logs:/workspace/logs \
+-v $DR_DIR/docker/volumes/.aws:/root/.aws \
+-v $DR_DIR/data/analysis:/workspace/analysis \
+-v $DR_DIR/data/minio:/workspace/minio \
 --name loganalysis \
 --network sagemaker-local \
  awsdeepracercommunity/deepracer-analysis:$DR_ANALYSIS_IMAGE
