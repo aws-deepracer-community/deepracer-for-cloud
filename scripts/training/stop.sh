b65c4eb6142c756bb9e36b009d5e56b03b49d751
Lars Ludvigsen <larsll@outlook.com>
2024-01-07 16:15:07 +0100

    Tune output when stopping

--- scripts/training/stop.sh
+++ scripts/training/stop.sh
@@ -14,7 +14,7 @@ then
         COMPOSE_FILE=$(sudo find /tmp/sagemaker -name docker-compose.yaml -exec grep -l "$RUN_NAME" {} + | grep $CONTAINER_PREFIX)
         if [[ -n $COMPOSE_FILE ]]; then
             sudo docker compose -f $COMPOSE_FILE stop $COMPOSE_SERVICE_NAME
-            docker container rm $CONTAINER
+            docker container rm $CONTAINER -v > /dev/null
         fi
     done
 fi
