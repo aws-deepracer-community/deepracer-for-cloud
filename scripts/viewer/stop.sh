35355c4976a75b90f38d1b67c6dbe698d7ac77ad
Lars Ludvigsen <larsll@outlook.com>
2024-01-09 22:06:04 +0100

    Fix viewer stop script

--- scripts/viewer/stop.sh
+++ scripts/viewer/stop.sh
@@ -8,5 +8,8 @@ if [[ "${DR_DOCKER_STYLE,,}" == "swarm" ]];
 then
     docker stack rm $STACK_NAME
 else
+    export DR_VIEWER_HTML=$DR_DIR/tmp/streams-$DR_RUN_ID.html
+    export DR_NGINX_CONF=$DR_DIR/tmp/streams-$DR_RUN_ID.conf
+
     docker compose -f $COMPOSE_FILES -p $STACK_NAME down
 fi
