8c3596337802e617b8c8ec8171b8a955c5002bb8
Lars Ludvigsen <larsll@outlook.com>
2024-01-25 08:08:33 +0100

    Tuning start script

--- utils/start-xorg.sh
+++ utils/start-xorg.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+set -e
+
+if [[ $EUID == 0 ]]; then
+    echo "ERROR: Do not run as root / via sudo."
+    exit 1
+fi
+
+if [ -z "$DR_DIR" ]; then
+    echo "ERROR: DR_DIR not set. Run 'source bin/activate.sh' before start-xorg.sh."
+    exit 1
+fi
+
+if [ -z "$DR_DISPLAY" ]; then
+    echo "ERROR: DR_DISPLAY not set. Ensure the variable is configured in system.env."
+    exit 1
+fi
+
+sudo touch $DR_DIR/tmp/xorg.log
+
 screen -dmS DeepracerXorg
 screen -r DeepracerXorg -X stuff $'sudo xinit /usr/bin/mwm -display $DR_DISPLAY -- /usr/lib/xorg/Xorg $DR_DISPLAY -config $DR_DIR/tmp/xorg.conf > $DR_DIR/tmp/xorg.log 2>&1 &\n'
 
