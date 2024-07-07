6eb3d6e1a2e12026721098c57fcd4dca2660502c
Lars Lorentz Ludvigsen <larsll@outlook.com>
2024-07-07 10:26:04 +0200

    XAUTHORITY not needed in WSL2

--- scripts/evaluation/start.sh
+++ scripts/evaluation/start.sh
@@ -34,6 +34,11 @@ while getopts ":qc" opt; do
   esac
 done
 
+## Check if WSL2
+if grep -qi Microsoft /proc/version && grep -q "WSL2" /proc/version; then
+    IS_WSL2="yes"
+fi
+
 # set evaluation specific environment variables
 STACK_NAME="deepracer-eval-$DR_RUN_ID"
 STACK_CONTAINERS=$(docker stack ps $STACK_NAME 2>/dev/null | wc -l)
@@ -86,7 +91,7 @@ if [[ "${DR_HOST_X,,}" == "true" ]]; then
     exit 1
   fi
 
-  if [[ -z "$XAUTHORITY" ]]; then
+  if [[ -z "$XAUTHORITY" && "$IS_WSL2" != "yes" ]]; then
     export XAUTHORITY=~/.Xauthority
     if [[ ! -f "$XAUTHORITY" ]]; then
       echo "No XAUTHORITY defined. .Xauthority does not exist. Stopping."
