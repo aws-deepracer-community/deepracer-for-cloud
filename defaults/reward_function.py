7e86eaeed255d8c64db8db1642e057a154805f97
Esther Hikari <110263916+eh1k4ri@users.noreply.github.com>
2024-11-08 08:08:38 -0300

    UPDATE: adding speed reward

--- defaults/reward_function.py
+++ defaults/reward_function.py
@@ -1,33 +1,35 @@
 def reward_function(params):
     '''
-    Example of penalize steering, which helps mitigate zig-zag behaviors
+    Reward function that considers both distance from center line and maintaining a moderate speed.
     '''
     
-    # Read input parameters
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    steering = abs(params['steering_angle']) e
+    speed = params['speed']
 
-    # Calculate 3 marks that are farther and father away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
 
-    # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1
+        reward = 1.0
     elif distance_from_center <= marker_2:
         reward = 0.5
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
-        reward = 1e-3  # likely crashed/ close to off track
+        reward = 1e-3 
 
-    # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 15
 
-    # Penalize reward if the car is steering too much
     if steering > ABS_STEERING_THRESHOLD:
         reward *= 0.8
+
+    SPEED_MIN = 1.5
+    SPEED_MAX = 2.5
+
+    if SPEED_MIN <= speed <= SPEED_MAX:
+        reward += 0.5 
 
     return float(reward)
