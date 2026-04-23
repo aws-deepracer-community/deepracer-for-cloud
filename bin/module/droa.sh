4f9606cf3ca1994a92108882bbed2dec6fff5351
Lars Ludvigsen <larsll@outlook.com>
2026-04-23 19:51:54 +0200

    Disable what is not tested

--- bin/module/droa.sh
+++ bin/module/droa.sh
@@ -2,7 +2,26 @@
 # DRoA (DeepRacer on AWS) shell functions.
 # Sourced by bin/activate.sh alongside scripts_wrapper.sh and summary.sh.
 
-function dr-droa-list-models {
+function droa-list-models {
   dr-update-env && python3 "${DR_DIR}/scripts/droa/list_models.py" "$@"
 }
 
+#function droa-get-model {
+#  dr-update-env && python3 "${DR_DIR}/scripts/droa/get_model.py" "$@"
+#}
+
+#function droa-get-logs {
+#  dr-update-env && python3 "${DR_DIR}/scripts/droa/get_logs.py" "$@"
+#}
+
+#function droa-trigger-evaluation {
+#  dr-update-env && python3 "${DR_DIR}/scripts/droa/trigger_evaluation.py" "$@"
+#}
+
+#function droa-delete-model {
+#  dr-update-env && python3 "${DR_DIR}/scripts/droa/delete_model.py" "$@"
+#}
+
+#function droa-import-model {
+#  dr-update-env && python3 "${DR_DIR}/scripts/droa/import_model.py" "$@"
+#}
